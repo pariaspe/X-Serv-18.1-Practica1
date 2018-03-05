18b2a853b2b23610c0bc9afc5c86d7c5606cfaf6
Pedro Arias Perez <parias@alpha28.aulas.gsyc.urjc.es>
2018-03-05 14:43:44 +0100

    Añadidos diccionarios

--- practica1.py
+++ practica1.py
@@ -9,6 +9,10 @@ Web recortadora de urls
 
 import webapp
 
+urls = {}
+nums = {}
+cont = 0
+
 class webShort(webapp.webApp):
     def parse(self, request):
         method = request.split()[0]
@@ -19,22 +23,33 @@ class webShort(webapp.webApp):
         resource = request.split()[1]
         return (method, resource, url)
 
+    def new_num(self):
+        global cont
+        cont += 1
+        return cont
+
     def process(self, parsedRequest):
         print(parsedRequest)
         if(parsedRequest[0] == "GET"):
             html_answer = """
                 <html>
                     <body>
-                        <h1>Formulario:</h1>
+                        <h1>Acortador de URLs</h1>
                         <form action="" method="POST">
-                            <label for="url">Name:</label><br>
-                            <input type="text" name="url" value="pepe"/><br>
+                            <label for="url">URL a acrotar:</label><br>
+                            <input type="text" name="url" value=""/><br>
                             <input type="submit" value="Send url">
                         </form>
                     </body>
                 </html>"""
         elif(parsedRequest[0] == "POST"):
-            html_answer = '<html><body><h1>URL: ' + parsedRequest[2] + '</h1></body></html>'
+            url = parsedRequest[2]
+            if url in urls:
+                print("ya dentro")
+            else:
+                urls[url] = self.new_num()
+
+            html_answer = '<html><body><h1>URL: ' + str(urls) + '</h1></body></html>'
         else:
             html_answer = "Error"
         return ("200 OK", html_answer)
